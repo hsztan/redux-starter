2f4076438ef9347f4390c6f3b8b3eaf1515c7541
Henry Nawrocki <hdev@sztanski.com>
2022-03-05 17:29:28 -0500

    Add middleware

--- src/index.js
+++ src/index.js
@@ -11,22 +11,21 @@ import { userAdded } from './store/users'
 
 const store = configureStore()
 
-store.subscribe(() => {
-  console.log('Store changed!')
+// store.subscribe(() => {
+//   console.log('Store changed!')
+// })
+
+store.dispatch((dispatch, getState) => {
+  //
+  dispatch({ type: 'bugsReceived', bugs: [1, 2, 3] })
+  console.log(getState())
 })
-
-store.dispatch(userAdded({ name: 'User 1' }))
-store.dispatch(userAdded({ name: 'User 2' }))
-store.dispatch(projectAdded({ name: 'Project 1' }))
-store.dispatch(projectAdded({ name: 'Project 1' }))
-store.dispatch(bugAdded({ description: 'Bug 1' }))
-store.dispatch(bugAdded({ description: 'Bug 2' }))
-store.dispatch(bugAdded({ description: 'Bug 3' }))
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
-store.dispatch(bugResolved({ id: 1 }))
-const x = getUnresolvedBugs(store.getState())
-const y = getUnresolvedBugs(store.getState())
-
-console.log(x === y)
-console.log(getBugsByUser(1)(store.getState()))
-console.log(store.getState())
+// store.dispatch(userAdded({ name: 'User 1' }))
+// store.dispatch(userAdded({ name: 'User 2' }))
+// store.dispatch(projectAdded({ name: 'Project 1' }))
+// store.dispatch(projectAdded({ name: 'Project 1' }))
+// store.dispatch(bugAdded({ description: 'Bug 1' }))
+// store.dispatch(bugAdded({ description: 'Bug 2' }))
+// store.dispatch(bugAdded({ description: 'Bug 3' }))
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
+// store.dispatch(bugResolved({ id: 1 }))
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
-// import reducer from './bugs'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import reducer from './reducer'
+import logger from './middleware/logger'
 
 export default function () {
-  return configureStore({ reducer })
+  return configureStore({
+    reducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(logger({ destination: 'console' })),
+  })
 }
